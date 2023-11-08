3274b97d3e31c4aa55dacab707637d358974ceba
vani.sahu <sahu.vani25@gmail.com>
2023-11-08 12:00:13 +0530

    Lets start coding done

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+    return  (
+        <div>
+            <div>food rs.10</div>
+            <div>
+                <div>petrol rs.100</div>
+                <div>movies rs.200</div>
+            </div>
+        </div>
+    );
+}
+export default ExpenseItem;
+  
