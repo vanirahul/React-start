336af3c97202840e463d092e38c3ec62fb4092b3
vani.sahu <sahu.vani25@gmail.com>
2023-12-19 14:54:07 +0530

    Rendering Expenses with performance optimisations

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -15,8 +15,19 @@ const Expenses = (props) => {
   return (
      <div>
     <Card className="expenses">
-        <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}
-      ExpenseItem
+        <ExpensesFilter 
+        selected={filteredYear} 
+        onChangeFilter={filterChangeHandler}/>
+
+      {props.items.map((expense) => (
+      <ExpenseItem 
+      key ={expense.id}
+      title={expense.title} 
+      amount={expense.amount} 
+      date={expense.date}
+      />))}
+{/*        
+      <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
         date={props.items[0].date}
@@ -35,10 +46,11 @@ const Expenses = (props) => {
         title={props.items[3].title}
         amount={props.items[3].amount}
         date={props.items[3].date}
-      />
+      /> */}
+      
     </Card>
     </div>
   );
-}
+};
 
 export default Expenses;
